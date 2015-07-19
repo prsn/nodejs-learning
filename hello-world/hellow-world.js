6cf48d5efc63af6fe75ed83f702d1577ea485969
Prasun Pal <prsn.pal@gmail.com>
2015-07-19 17:26:05 +0530

    hello-world program

---
+++ hello-world/hellow-world.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-type': 'text/plain'});
+	response.end('Hello world\n');
+	
+}).listen(8525);
+console.log('Server listening at port no 8525');
